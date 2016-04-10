5cc636ef137c031ef2dc1d71b29d09f03bcf9c72
gastoncattani <gastoncattani@gmail.com>
2016-04-09 22:47:07 -0300

    Prueba

---
+++ BLL/blAutor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class blAutor
+    {
+        public int MyProperty { get; set; }
+		public int MyProperty { get; set; }
+    }
+}
